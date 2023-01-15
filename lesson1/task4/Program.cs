5f744716aabfaf492a751dc4c8c0bbe59b9ab74a
MikhailZubkov <toothkov@gmail.com>
2023-01-15 23:31:32 +0500

    DZ 1st seminar Cs

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите число");
+string Nstr = Console.ReadLine();
+int N = Convert.ToInt32(Nstr);
+for (int i = 2; i <= N; i+=2)
+{
+    Console.Write(i+" ");
+}
