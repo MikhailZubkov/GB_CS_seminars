13e5615006eafb16feb74ae01cfd9d636f743fd4
MikhailZubkov <toothkov@gmail.com>
2023-02-07 20:43:46 +0500

    dz lesson6

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+вводим через один Enter (разбиваем через Split())
+*/
+
+
+int [] ArrayOfString (string text)
+{
+    char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+
+    string[] words = text.Split(delimiterChars);
+    int [] array = new int [words.Length];
+
+    for (int i = 0; i < words.Length - 1; i++)
+    {
+        array[i] = Convert.ToInt32(words[i]);
+    }
+    Console.Write(string.Join(", ", array));
+    return array;
+}
+
+int PositiveNumbers(int [] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+            quantity++;
+    }
+
+    return quantity;
+}
+
+Console.WriteLine("Введите числа: ");
+string text = Console.ReadLine();
+int [] array = ArrayOfString(text);
+int quantity = PositiveNumbers(array);
+
+Console.WriteLine($" -> Чисел больше нуля: {quantity}");
---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+(double, double) PolucheniyeCoordinat (int b1, int k1, int b2, int k2)
+{
+    double x = (double)(b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+Console.WriteLine("Введите значение b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+(double x, double y) = PolucheniyeCoordinat(b1, k1, b2, k2);
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
