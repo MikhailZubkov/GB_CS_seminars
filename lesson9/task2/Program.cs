3493fce9a0c474beb6a4c61916bc879e8f7bfa36
MikhailZubkov <toothkov@gmail.com>
2023-02-17 21:14:08 +0500

    урок 9

---
+++ lesson9/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumOfNumbers (int m, int n)
+{
+    if(m == n)
+        return n;
+    else 
+        return m + SumOfNumbers(m + 1, n);
+    
+}
+
+int sum = SumOfNumbers(4, 8);
+Console.WriteLine(sum);
---
+++ lesson9/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int GetNumber(string message){
+        int result = 0;
+    while (true){
+            Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result)){
+            break;
+        }        
+        else{    
+            Console.WriteLine("Вы ввели некорректное число");
+        }
+    }
+    return result;
+}
+int ack(int n, int m)
+{
+    if(n == 0)
+        return m + 1;
+    else
+        if(m == 0)
+            return ack (n - 1, 1);    
+        else
+            return ack(n - 1, ack (n, m - 1));
+}
+int a = GetNumber("Enter positive number");
+int b = GetNumber("Enter positive number");
+Console.WriteLine(ack(a, b));
